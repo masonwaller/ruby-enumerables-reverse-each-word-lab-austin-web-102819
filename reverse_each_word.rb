e84f3664837912a771b705e5d5b6a04c7e0d02df
Mason Waller <masonwaller805@gmail.com>
2019-10-15 23:45:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1) 
 array= sentence1.split(/ /)
 string=[]
-array.each { |n| string.push("#{n}".reverse)}
+array.collect { |n| string.push("#{n}".reverse)}
 return string.join(" ")
 end
